972bae13807202b798cbe8a8d71f01971cde09bb
Lanny Qin <lanny.qin@qq.com>
2025-08-16 19:38:39 +0800

    清理提交

--- test/test_wifi.py
+++ test/test_wifi.py
@@ -5,3 +5,6 @@ wifi = network.WLAN(network.STA_IF)
 wifi.active(True)  # 观察是否还会触发复位
 print("Wi-Fi 模块已激活")
 # wifi.connect("SSID", "PASSWORD")  # 可后续再试
+wifi.connect('Xiaomi_Stream','1qazxsw20plmnko9')
+sleep(5)
+print(wifi.isconnected())
